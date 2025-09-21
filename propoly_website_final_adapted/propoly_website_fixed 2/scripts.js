d7fa60cc3f7cd18ecf14e311ffb4014c83b70375
mouadhilali01-dotcom <mouadhilali01@gmail.com>
2025-09-21 20:22:51 +0100

    Delete propoly_website_final_adapted/propoly_website_fixed 2/scripts.js

--- propoly_website_final_adapted/propoly_website_fixed 2/scripts.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{
-  const toggle=document.querySelector(".menu-toggle");
-  const navMenu=document.querySelector(".main-nav ul");
-  toggle.addEventListener("click",()=>{navMenu.classList.toggle("active");});
-});
